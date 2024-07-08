82f0e6cdaddd67075f34a9eb4e4252c489530685
suhasledange <suhasledange10@gmail.com>
2024-07-08 16:04:00 +0530

    routes setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,8 @@
-const asyncHandler = (requestHandler)=> async (req,res,next)=>{
-    
+const asyncHandler = (requestHandler)=> {
+    return (req,res,next)=>{
     Promise.resolve(requestHandler(req,res,next))
     .catch((err)=>next(err))
-
+}
 }
 
 export {asyncHandler}
